6e41c039ef6e8595cf31333d0e9b1d6fcc65d3ce
Edgar Torres <edgar-yoel.torres-rios@ipvs.uni-stuttgart.de>
2024-09-12 21:12:18 +0200

    added hyper-lr-pinn

---
+++ src/models/hyper_lr_pinn.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+
+class LR_PINN(nn.Module):
+    def __init__(self, hidden_dim, phase='phase1', start_w=None, start_b=None, end_w=None, end_b=None,
+                 col_0=None, col_1=None, col_2=None, row_0=None, row_1=None, row_2=None,
+                 alpha_0=None, alpha_1=None, alpha_2=None):
+        super(LR_PINN, self).__init__()
+
+        # Common layers
+        self.start_layer = nn.Linear(2, hidden_dim)
+        self.end_layer = nn.Linear(hidden_dim, 1)
+
+        self.hidden_dim = hidden_dim
+        self.scale = 1 / hidden_dim
+
+        # Phase1-specific parameters
+        if phase == 'phase1':
+            self.col_basis_0 = nn.Parameter(self.scale * torch.rand(self.hidden_dim, self.hidden_dim))
+            self.col_basis_1 = nn.Parameter(self.scale * torch.rand(self.hidden_dim, self.hidden_dim))
+            self.col_basis_2 = nn.Parameter(self.scale * torch.rand(self.hidden_dim, self.hidden_dim))
+
+            self.row_basis_0 = nn.Parameter(self.scale * torch.rand(self.hidden_dim, self.hidden_dim))
+            self.row_basis_1 = nn.Parameter(self.scale * torch.rand(self.hidden_dim, self.hidden_dim))
+            self.row_basis_2 = nn.Parameter(self.scale * torch.rand(self.hidden_dim, self.hidden_dim))
+
+            self.meta_layer_1 = nn.Linear(1, self.hidden_dim)  # Single parameter instead of three
+            self.meta_layer_2 = nn.Linear(self.hidden_dim, self.hidden_dim)
+            self.meta_layer_3 = nn.Linear(self.hidden_dim, self.hidden_dim)
+
+            self.meta_alpha_0 = nn.Linear(self.hidden_dim, self.hidden_dim)
+            self.meta_alpha_1 = nn.Linear(self.hidden_dim, self.hidden_dim)
+            self.meta_alpha_2 = nn.Linear(self.hidden_dim, self.hidden_dim)
+
+        # Phase2-specific parameters
+        if phase == 'phase2':
+            self.start_layer.weight = nn.Parameter(start_w)
+            self.start_layer.bias = nn.Parameter(start_b)
+            self.end_layer.weight = nn.Parameter(end_w)
+            self.end_layer.bias = nn.Parameter(end_b)
+
+            self.col_basis_0 = nn.Parameter(col_0, requires_grad=False)
+            self.col_basis_1 = nn.Parameter(col_1, requires_grad=False)
+            self.col_basis_2 = nn.Parameter(col_2, requires_grad=False)
+
+            self.row_basis_0 = nn.Parameter(row_0, requires_grad=False)
+            self.row_basis_1 = nn.Parameter(row_1, requires_grad=False)
+            self.row_basis_2 = nn.Parameter(row_2, requires_grad=False)
+
+            self.alpha_0 = nn.Parameter(alpha_0)
+            self.alpha_1 = nn.Parameter(alpha_1)
+            self.alpha_2 = nn.Parameter(alpha_2)
+
+        self.tanh = nn.Tanh()
+        self.relu = nn.ReLU()
+
+        self.phase = phase
+
+    def forward_phase1(self, x, t, meta_param):
+        # Meta-learning part (Phase1)
+        meta_output = self.meta_layer_1(meta_param)
+        meta_output = self.tanh(meta_output)
+        meta_output = self.meta_layer_2(meta_output)
+        meta_output = self.tanh(meta_output)
+        meta_output = self.meta_layer_3(meta_output)
+        meta_output = self.tanh(meta_output)
+
+        meta_alpha_0_output = self.relu(self.meta_alpha_0(meta_output))
+        meta_alpha_1_output = self.relu(self.meta_alpha_1(meta_output))
+        meta_alpha_2_output = self.relu(self.meta_alpha_2(meta_output))
+
+        alpha_0 = torch.diag_embed(meta_alpha_0_output)
+        alpha_1 = torch.diag_embed(meta_alpha_1_output)
+        alpha_2 = torch.diag_embed(meta_alpha_2_output)
+
+        # Main neural network (Phase1)
+        inputs = torch.cat([x, t], axis=1)
+        weight_0 = torch.matmul(torch.matmul(self.col_basis_0, alpha_0), self.row_basis_0)
+        weight_1 = torch.matmul(torch.matmul(self.col_basis_1, alpha_1), self.row_basis_1)
+        weight_2 = torch.matmul(torch.matmul(self.col_basis_2, alpha_2), self.row_basis_2)
+
+        emb_out = self.start_layer(inputs)
+        emb_out = self.tanh(emb_out)
+        emb_out = emb_out.unsqueeze(dim=1)
+
+        emb_out = torch.bmm(emb_out, weight_0)
+        emb_out = self.tanh(emb_out)
+
+        emb_out = torch.bmm(emb_out, weight_1)
+        emb_out = self.tanh(emb_out)
+
+        emb_out = torch.bmm(emb_out, weight_2)
+        emb_out = self.tanh(emb_out)
+
+        emb_out = self.end_layer(emb_out)
+        emb_out = emb_out.squeeze(dim=1)
+
+        return emb_out
+
+    def forward_phase2(self, x, t, meta_param=None):
+        # Main neural network (Phase2)
+        weight_0 = torch.matmul(torch.matmul(self.col_basis_0, torch.diag(self.alpha_0)), self.row_basis_0)
+        weight_1 = torch.matmul(torch.matmul(self.col_basis_1, torch.diag(self.alpha_1)), self.row_basis_1)
+        weight_2 = torch.matmul(torch.matmul(self.col_basis_2, torch.diag(self.alpha_2)), self.row_basis_2)
+
+        inputs = torch.cat([x, t], axis=1)
+        emb_out = self.start_layer(inputs)
+        emb_out = self.tanh(emb_out)
+
+        emb_out = torch.matmul(emb_out, weight_0)
+        emb_out = self.tanh(emb_out)
+
+        emb_out = torch.matmul(emb_out, weight_1)
+        emb_out = self.tanh(emb_out)
+
+        emb_out = torch.matmul(emb_out, weight_2)
+        emb_out = self.tanh(emb_out)
+
+        emb_out = self.end_layer(emb_out)
+        return emb_out
+
+    def forward(self, x, t, meta_param):
+        if self.phase == 'phase1':
+            return self.forward_phase1(x, t, meta_param)
+        elif self.phase == 'phase2':
+            return self.forward_phase2(x, t)
+
+    def get_phase2_weights(self, meta_param):
+        """Extracts weights from Phase1 to initialize Phase2"""
+        # Meta-learning to get the alphas (same as in Phase1 forward)
+        meta_output = self.meta_layer_1(meta_param)
+        meta_output = self.tanh(meta_output)
+        meta_output = self.meta_layer_2(meta_output)
+        meta_output = self.tanh(meta_output)
+        meta_output = self.meta_layer_3(meta_output)
+        meta_output = self.tanh(meta_output)
+
+        meta_alpha_0_output = self.relu(self.meta_alpha_0(meta_output))
+        meta_alpha_1_output = self.relu(self.meta_alpha_1(meta_output))
+        meta_alpha_2_output = self.relu(self.meta_alpha_2(meta_output))
+
+        alpha_0 = meta_alpha_0_output.squeeze()
+        alpha_1 = meta_alpha_1_output.squeeze()
+        alpha_2 = meta_alpha_2_output.squeeze()
+
+        # Collect weights for Phase2 initialization
+        return {
+            'start_w': self.start_layer.weight.data.clone(),
+            'start_b': self.start_layer.bias.data.clone(),
+            'end_w': self.end_layer.weight.data.clone(),
+            'end_b': self.end_layer.bias.data.clone(),
+            'col_0': self.col_basis_0.data.clone(),
+            'col_1': self.col_basis_1.data.clone(),
+            'col_2': self.col_basis_2.data.clone(),
+            'row_0': self.row_basis_0.data.clone(),
+            'row_1': self.row_basis_1.data.clone(),
+            'row_2': self.row_basis_2.data.clone(),
+            'alpha_0': alpha_0,
+            'alpha_1': alpha_1,
+            'alpha_2': alpha_2
+        }
